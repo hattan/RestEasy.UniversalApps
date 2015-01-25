20058e915ef96894a7a3d04e59b7f3b2da1e9cca
Hattan Shobokshi <hattan@gmail.com>
2015-01-24 21:55:43 -0800

    initial commit for universal portable class lib

---
+++ RestEasy/RestClient.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+
+namespace RestEasy
+{
+    public class RestClient
+    {
+
+        public bool IsInDeignModel
+        {
+            get
+            {
+                return Windows.ApplicationModel.DesignMode.DesignModeEnabled;
+            }
+        }
+
+        private static string ComputeMd5(string str)
+        {
+            var alg = HashAlgorithmProvider.OpenAlgorithm("MD5");
+            IBuffer buff = CryptographicBuffer.ConvertStringToBinary(str, BinaryStringEncoding.Utf8);
+            var hashed = alg.HashData(buff);
+            var res = CryptographicBuffer.EncodeToHexString(hashed);
+            return res;
+        }
+
+        public async Task<T> GetAsync<T>(string requestUri, Dictionary<string, string> headers = null)
+        {
+            string key = ComputeMd5(requestUri);
+
+            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+            {
+                if (await StorageHelper.FileExistsAsync(key))
+                {
+                    var result = await StorageHelper.ReadFileAsync(key);
+                    return JsonConvert.DeserializeObject<T>(result);
+                }
+            }
+
+            var response = await FetchLiveData(requestUri, headers);
+
+            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+            {
+                SaveResponseToDisk(key, response);
+            }
+
+            return JsonConvert.DeserializeObject<T>(response.Trim());
+        }
+
+        private static async Task<string> FetchLiveData(string requestUri, Dictionary<string, string> headers)
+        {
+            var httpClient = new HttpClient().AddHeaders(headers);
+            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(requestUri);
+            string response = await httpResponseMessage.Content.ReadAsStringAsync();
+            return response;
+        }
+
+        private static void SaveResponseToDisk(string key, string response)
+        {
+            StorageHelper.WriteFileFireAndForget(key, response.Trim(), StorageHelper.StorageStrategies.Local);
+        }
+
+        public async Task<T> PostAsync<T>(string requestUri, Dictionary<string, string> headers = null, Dictionary<string, string> parameters = null)
+        {
+            return await PostAsync<T>(requestUri, headers, parameters != null ? FormatPostParameters(parameters) : null);
+        }
+
+        public async Task<T> PostAsync<T>(string requestUri, Dictionary<string, string> headers = null, string content = null)
+        {
+            var client = new HttpClient().AddHeaders(headers);
+
+            HttpContent httpContent = new StringContent(content ?? "");
+            httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+            HttpResponseMessage httpResponseMessage = await client.PostAsync(requestUri, httpContent);
+            string response = await httpResponseMessage.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(response);
+        }
+
+        protected string FormatPostParameters(Dictionary<string, string> parameters)
+        {
+            var result = "";
+            int count = 0;
+            foreach (var parameter in parameters)
+            {
+                if (count > 0)
+                {
+                    result = result + "&";
+                }
+                result = result + parameter.Key + "=" + parameter.Value;
+                count++;
+            }
+            return result;
+        }
+    }
+
+    public static class HttpClientExtensions
+    {
+        public static HttpClient AddHeaders(this HttpClient httpClient, Dictionary<string, string> headers)
+        {
+            if (headers != null)
+            {
+                foreach (var header in headers)
+                {
+                    httpClient.DefaultRequestHeaders.Add(header.Key, header.Value);
+                }
+            }
+            return httpClient;
+        }
+    }
+}
---
+++ RestEasy/StorageHelper.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Windows.Storage;
+
+namespace RestEasy
+{
+    /*Fork of the StorageHelper class created by Jerry Nixon. I've made a few small modifications
+     * including using Json.net to serialize/deserialize data
+     * http://blog.jerrynixon.com/2012/06/windows-8-how-to-read-files-in-winrt.html
+     * */
+    public static class StorageHelper
+    {
+        #region Settings
+
+        public static bool SettingExists(string key, StorageStrategies location = StorageStrategies.Local)
+        {
+            switch (location)
+            {
+                case StorageStrategies.Local:
+                    return ApplicationData.Current.LocalSettings.Values.ContainsKey(key);
+                case StorageStrategies.Roaming:
+                    return ApplicationData.Current.RoamingSettings.Values.ContainsKey(key);
+                default:
+                    throw new NotSupportedException(location.ToString());
+            }
+        }
+
+        public static T GetSetting<T>(string key, T otherwise = default(T),
+                                      StorageStrategies location = StorageStrategies.Local)
+        {
+            try
+            {
+                if (!(SettingExists(key, location)))
+                    return otherwise;
+
+                switch (location)
+                {
+                    case StorageStrategies.Local:
+                        return (T)ApplicationData.Current.LocalSettings.Values[key];
+                    case StorageStrategies.Roaming:
+                        return (T)ApplicationData.Current.RoamingSettings.Values[key];
+                    default:
+                        throw new NotSupportedException(location.ToString());
+                }
+            }
+            catch
+            {
+                return otherwise;
+            }
+        }
+
+        public static void SetSetting<T>(string key, T value, StorageStrategies location = StorageStrategies.Local)
+        {
+            switch (location)
+            {
+                case StorageStrategies.Local:
+                    ApplicationData.Current.LocalSettings.Values[key] = value;
+                    break;
+                case StorageStrategies.Roaming:
+                    ApplicationData.Current.RoamingSettings.Values[key] = value;
+                    break;
+                default:
+                    throw new NotSupportedException(location.ToString());
+            }
+        }
+
+        public static void DeleteSetting(string key, StorageStrategies location = StorageStrategies.Local)
+        {
+            switch (location)
+            {
+                case StorageStrategies.Local:
+                    ApplicationData.Current.LocalSettings.Values.Remove(key);
+                    break;
+                case StorageStrategies.Roaming:
+                    ApplicationData.Current.RoamingSettings.Values.Remove(key);
+                    break;
+                default:
+                    throw new NotSupportedException(location.ToString());
+            }
+        }
+
+        #endregion
+
+        #region File
+
+        public static async Task<bool> FileExistsAsync(string key, StorageStrategies location = StorageStrategies.Local)
+        {
+            return (await GetIfFileExistsAsync(key, location)) != null;
+        }
+
+        public static async Task<bool> FileExistsAsync(string key, StorageFolder folder)
+        {
+            return (await GetIfFileExistsAsync(key, folder)) != null;
+        }
+
+        public static async Task<bool> DeleteFileAsync(string key, StorageStrategies location = StorageStrategies.Local)
+        {
+            StorageFile file = await GetIfFileExistsAsync(key, location);
+            if (file != null)
+                await file.DeleteAsync();
+            return !(await FileExistsAsync(key, location));
+        }
+
+        public static async Task<string> ReadFileAsync(string key, StorageStrategies location = StorageStrategies.Local)
+        {
+            // fetch file
+            StorageFile file = await GetIfFileExistsAsync(key, location);
+
+            // read content
+            string _String = await FileIO.ReadTextAsync(file);
+
+            return _String;
+        }
+
+        public static async Task<T> ReadFileAsync<T>(string key, StorageStrategies location = StorageStrategies.Local)
+        {
+            // fetch file
+            StorageFile file = await GetIfFileExistsAsync(key, location);
+            if (file == null)
+                return default(T);
+            // read content
+            string _String = await FileIO.ReadTextAsync(file);
+            // convert to obj
+            var result = Deserialize<T>(_String);
+            return result;
+        }
+
+        public static async Task<bool> WriteFileAsync(string key, string body,
+                                                      StorageStrategies location = StorageStrategies.Local)
+        {
+            // create file
+            StorageFile file = await CreateFileAsync(key, location, CreationCollisionOption.ReplaceExisting);
+
+            await FileIO.WriteTextAsync(file, body);
+            // result
+            return await FileExistsAsync(key, location);
+        }
+
+        public static async Task<bool> WriteFileAsync<T>(string key, T value,
+                                                         StorageStrategies location = StorageStrategies.Local)
+        {
+            // create file
+            StorageFile file = await CreateFileAsync(key, location, CreationCollisionOption.ReplaceExisting);
+            // convert to string
+            string _String = Serialize(value);
+            // save string to file
+            await FileIO.WriteTextAsync(file, _String);
+            // result
+            return await FileExistsAsync(key, location);
+        }
+
+        private static async Task<StorageFile> CreateFileAsync(string key,
+                                                               StorageStrategies location = StorageStrategies.Local,
+                                                               CreationCollisionOption option =
+                                                                   CreationCollisionOption.OpenIfExists)
+        {
+            switch (location)
+            {
+                case StorageStrategies.Local:
+                    return await ApplicationData.Current.LocalFolder.CreateFileAsync(key, option);
+                case StorageStrategies.Roaming:
+                    return await ApplicationData.Current.RoamingFolder.CreateFileAsync(key, option);
+                case StorageStrategies.Temporary:
+                    return await ApplicationData.Current.TemporaryFolder.CreateFileAsync(key, option);
+                default:
+                    throw new NotSupportedException(location.ToString());
+            }
+        }
+
+        private static async Task<StorageFile> GetIfFileExistsAsync(string key, StorageFolder folder)
+        {
+            StorageFile retval;
+            try
+            {
+                retval = await folder.GetFileAsync(key);
+            }
+            catch (FileNotFoundException)
+            {
+                Debug.WriteLine("GetIfFileExistsAsync:FileNotFoundException");
+                return null;
+            }
+            return retval;
+        }
+
+
+        private static async Task<StorageFile> GetIfFileExistsAsync(string key,
+                                                                    StorageStrategies location = StorageStrategies.Local)
+        {
+            StorageFile retval;
+            try
+            {
+                switch (location)
+                {
+                    case StorageStrategies.Local:
+                        retval = await ApplicationData.Current.LocalFolder.GetFileAsync(key);
+                        break;
+                    case StorageStrategies.Roaming:
+                        retval = await ApplicationData.Current.RoamingFolder.GetFileAsync(key);
+                        break;
+                    case StorageStrategies.Temporary:
+                        retval = await ApplicationData.Current.TemporaryFolder.GetFileAsync(key);
+                        break;
+                    default:
+                        throw new NotSupportedException(location.ToString());
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Debug.WriteLine("GetIfFileExistsAsync:FileNotFoundException");
+                return null;
+            }
+
+            return retval;
+        }
+
+        #endregion
+
+        public enum StorageStrategies
+        {
+            Local,
+            Roaming,
+            Temporary
+        }
+
+        private static string Serialize(object objectToSerialize)
+        {
+            return JsonConvert.SerializeObject(objectToSerialize);
+        }
+
+        private static T Deserialize<T>(string jsonString)
+        {
+            return JsonConvert.DeserializeObject<T>(jsonString);
+        }
+
+        public static async void DeleteFileFireAndForget(string key, StorageStrategies location)
+        {
+            await DeleteFileAsync(key, location);
+        }
+
+        public static async void WriteFileFireAndForget(string key, string body, StorageStrategies location)
+        {
+            await WriteFileAsync(key, body, location);
+        }
+
+        public static async void WriteFileFireAndForget<T>(string key, T value, StorageStrategies location)
+        {
+            await WriteFileAsync(key, value, location);
+        }
+    }
+}
